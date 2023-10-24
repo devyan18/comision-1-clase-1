90ace648c910d7bf9e00ac1d7b12bf0297bd46c0
devyan18 <epache17@gmail.com>
2023-10-23 21:04:38 -0300

    read file asyncrono

--- app.js
+++ app.js
@@ -1,35 +1,12 @@
-const numeros = [1,20,3,4,5,6,7,8,20,10,11,12] // -> puede ser lo que obtenga de la base datos
+const fs = require("node:fs/promises")
 
-// for (let i = 0;  i < numeros.length; i++) {
-//   if (!(numeros[i] % 2)) {
-//     pares.push(numeros[i])
-//   }
-// }
+async function leerArchivo () {
+  try {
+    const data = await fs.readFile("./archivo.txt", "utf-8")
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+} 
 
-// Filter
-const pares = numeros.filter((num) => !(num % 2))
-
-
-// for (let i = 0;  i < numeros.length; i++) {
-//  doble.push(numeros[i] * 2)
-// }
-
-// Map
-const doble = numeros.map((num) => num * 2)
-
-
-// forEach
-numeros.forEach((num) => {
-  
-})
-
-// Find
-
-let elNumero = "20asdasd"
-
-if (!isNaN(+elNumero)) {
-  let miNumero = numeros.find((num) => num === +elNumero)
-  console.log(miNumero)
-}
-
-
+leerArchivo()
